d3cbe82e1d01507fdc474ced835daed63dbdc74e
Artyom <awesomemdl95@gmail.com>
2023-03-05 15:58:30 +0300

    pages

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="container">
+		<div class="notfound">
+			<h1>Page not found</h1>
+			<a href="/">Go to home</a>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.notfound {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	height: calc(100vh - var(--header-height) - var(--main-padding) * 2);
+	background: var(--content-background);
+}
+</style>
